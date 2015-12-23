12624a738408542f56d1fd6ee2dd6c31c3e64c59
Wang,Dawei <dwang@entertainment.com>
2015-12-23 17:27:00 +0000

    Use getvar for strict_variables=true

--- manifests/maven.pp
+++ manifests/maven.pp
@@ -39,7 +39,7 @@ class maven::maven(
   $archive = "/tmp/apache-maven-${version}-bin.tar.gz"
   
   # Avoid redownloading when tmp tar.gz is deleted
-  if getvar($::maven_version) != $version {
+  if getvar('$::maven_version') != $version {
 
     # we could use puppet-stdlib function !empty(repo) but avoiding adding a new
     # dependency for now
